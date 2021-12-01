1b3edbf5f6174e4b0a9bca236a59956492975378
jscharpff <jcdscharpff@gmail.com>
2021-12-01 08:22:35 +0100

    First challenge of AoC2021

---
+++ src/challenges/day01/Day1.java
@@ -0,0 +1,49 @@
+package challenges.day01;
+
+import util.io.FileReader;
+
+public class Day1 {
+
+	/**
+	 * Day 1 of the Advent of Code 2021
+	 * 
+	 * https://adventofcode.com/2021/day/1
+	 * 
+	 * @param args The command line arguments
+	 * @throws Exception
+	 */
+	public static void main( final String[] args ) throws Exception {
+		final int[] ex_input = new FileReader( Day1.class.getResource( "day1_example.txt" ) ).readIntArray( );
+		final int[] input = new FileReader( Day1.class.getResource( "day1_input.txt" ) ).readIntArray( );
+		
+		System.out.println( "---[ Part 1 ]---" );
+		System.out.println( "Example: " + countIncreases( ex_input, 1 ) ); 
+		System.out.println( "Answer: " + countIncreases( input, 1 ) ); 
+
+		System.out.println( "\n---[ Part 2 ]---" );
+		System.out.println( "Example: " + countIncreases( ex_input, 3 ) ); 
+		System.out.println( "Answer: " + countIncreases( input, 3 ) ); 
+	}
+	
+	/**
+	 * Count how many times the value increases for every two successive array
+	 * windows of n elements
+	 * 
+	 * @param input Array of integer inputs
+	 * @param n The window size
+	 * @return The number of times an increase is observed between elements
+	 */
+	private static long countIncreases( final int[] input, final int n ) {
+		long inccount = 0;
+		for( int i = n; i < input.length; i++ ) {
+			int sumA = 0; int sumB = 0;
+			for( int j = i - n + 1; j <= i; j++ ) {
+				sumA += input[j-1];
+				sumB += input[j];
+			}
+			if( sumB > sumA ) inccount++;
+		}
+
+		return inccount;
+	}
+}
