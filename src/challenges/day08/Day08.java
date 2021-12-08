3cce6b8239df943c600c91c6903300d014b3290c
jscharpff <jcdscharpff@gmail.com>
2021-12-08 12:26:10 +0100

    Challenge day 8

---
+++ src/challenges/day08/Day08.java
@@ -0,0 +1,72 @@
+package challenges.day08;
+
+import java.util.List;
+
+import util.io.FileReader;
+
+public class Day08 {
+
+	/**
+	 * Day 8 of the Advent of Code 2021
+	 * 
+	 * https://adventofcode.com/2021/day/8
+	 * 
+	 * @param args The command line arguments
+	 * @throws Exception
+	 */
+	public static void main( final String[] args ) throws Exception {
+		final List<String> ex_input = new FileReader( Day08.class.getResource( "day08_example.txt" ) ).readLines();
+		final List<String> input = new FileReader( Day08.class.getResource( "day08_input.txt" ) ).readLines( );
+		
+		System.out.println( "---[ Part 1 ]---" );
+		System.out.println( "Example: " + part1( ex_input ) );
+		System.out.println( "Answer : " + part1( input ) );
+
+		System.out.println( "\n---[ Part 2 ]---" );
+		System.out.println( "Example: " + part2( ex_input ) );
+		System.out.println( "Answer : " + part2( input ) );
+	}
+	
+	/**
+	 * Part 1 is easy, simply count all 2, 3, 4 or 7 length string in the output
+	 * digit part of the input and return the count 
+	 * 
+	 * @param input The list of digit-encoding segments
+	 * @return The count of "unique" digits in the output
+	 */
+	public static long part1( final List<String> input ) {
+		// part 1 is easy, just count the number of 2, 3, 4 and 7 length segments
+		// in the output digits
+		int count = 0;
+		for( final String s : input ) {
+			final String out = s.split( " \\| " )[1];
+			for( final String d : out.split( " " ) ) {
+				final int len = d.length( );
+				if( len == 2 || len == 3 || len == 4 || len == 7 ) count++;
+			}
+		}
+		
+		return count;
+	}
+	
+	/**
+	 * 
+	 * @param input
+	 * @return
+	 */
+	public static long part2( final List<String> input ) {
+		// decode each line and sum over their output
+		long sum = 0;
+		for( final String s : input ) {
+			// split each input into input and output part
+			final String[] enc = s.split( " \\| " );
+			
+			// use input part to initialise decoder
+			 final DigitDecoder d = new DigitDecoder( enc[0].split( " " ) );
+			 
+			 // and use decoder to decode output part
+			 sum += d.decode( enc[1].split( " " ) );
+		}
+		return sum;
+	}
+}
---
+++ src/challenges/day08/DigitDecoder.java
@@ -0,0 +1,164 @@
+package challenges.day08;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import util.Util;
+
+/**
+ * Class that can decode character-encoded digits by inferring the encoding
+ * from a set of input signals
+ * 
+ * @author Joris
+ */
+public class DigitDecoder {
+	/** The original input */
+	protected final List<String> input;
+	
+	/** The map of all segments in the input and their decoded value */
+	protected final Map<Integer, String> digits;
+
+	/**
+	 * Creates a new DigitEncoder
+	 * 
+	 * @param strinput The initial input that contains the segment encoding for
+	 *   all 10 digits from zero to nine
+	 */
+	public DigitDecoder( final String[] strinput ) {
+		if( strinput.length != 10 ) 
+			throw new RuntimeException( "Invalid segment encoding as input, exactly 10 segments are required."  );
+		
+		digits = new HashMap<>( 10 );
+		input = Arrays.asList( strinput );
+
+		buildMapping( );
+	}
+	
+	/**
+	 * Reconstructs the digit mapping from the input samples
+	 */
+	protected void buildMapping( ) {
+		// list of remaining input strings, discarding the output part for now
+		final List<String> remaining = new ArrayList<>( input );
+		
+		// get input segments that lead to unique digits 	
+		for( final String s : input ) {
+			if( s.length( ) == 2 ) { found( remaining, s, 1 ); continue; }
+			if( s.length( ) == 3 ) { found( remaining, s, 7 ); continue; }
+			if( s.length( ) == 4 ) { found( remaining, s, 4 ); continue; }
+			if( s.length( ) == 7 ) { found( remaining, s, 8 ); continue; }
+		}
+		
+		// now deduce digit encodings from segment descriptions
+		
+		// the segment diff between 8 and 1 can only be in 6
+		final String diff81 = Util.stringDifference( digits.get( 8 ), digits.get( 1 ) );
+		found( remaining, find( remaining, diff81, -1, true ), 6);
+		
+		// of the now remaining digits, the diff between 8 and 4 can now only be 2 or 0
+		final String diff84 = Util.stringDifference( digits.get( 8 ), digits.get( 4 ) );
+		found( remaining, find( remaining, diff84, 6, true ), 0 );
+		found( remaining, find( remaining, diff84, 5, true ), 2 );
+		
+		// of the remaining digits, the only one that does NOT have the difference between 8 and 2 is 3
+		final String diff82 = Util.stringDifference( digits.get( 8 ), digits.get( 2 ) );
+		found( remaining, find( remaining, diff82,-1, false ), 3 );
+		
+		// finally, 5 and 9 can be discerned by their length
+		for( int i = remaining.size( ) - 1; i >= 0; i-- ) {
+			final String s = remaining.get( i );
+			if( s.length( ) == 5 ) found( remaining, s, 5 );
+			else if( s.length( ) == 6 ) found( remaining, s, 9 );
+		}
+		
+		// check length
+		if( remaining.size( ) > 0 ) throw new RuntimeException( "Not all digits have been decoded, remaining: " + remaining );
+	}
+	
+	/**
+	 * Sets the encoding for the given digit and removes it from the remaining list
+	 * 
+	 * @param remaining The list of digits remaining
+	 * @param encoding The segment encoding of the digit 
+	 * @param digit The integer value of the digit to store
+	 */
+	protected void found( final List<String> remaining, final String encoding, final int digit ) {
+		digits.put( digit, encoding );
+		remaining.remove( encoding );
+	}
+	
+	/**
+	 * Goes over all remaining segment encodings to find the segment code to 
+	 * match/not match. Returns the segment encoding.
+	 * 
+	 * Parameters are length to consider only segments of a given size and
+	 * shouldmatch to specify whether the segment should or should not contain
+	 * all of the segments in the match string
+	 * 
+	 * @param remaining The list of remaining encoded segments
+	 * @param match The string of segments to match
+	 * @param digit The digit that we are looking for
+	 * @param length The number of segments we are looking for in the encoding. 
+	 *   Can be -1 for any length
+	 * @param shouldmatch True to require all segments of match to be present, 
+	 *   false to require that none of the segments of match are present
+	 * @return The segment encoding that matches the filter criteria
+	 */
+	protected String find( final List<String> remaining, final String match, final int length, final boolean shouldmatch ) {
+		for( int i = remaining.size( ) - 1; i >= 0; i-- ) {
+			final String s = remaining.get( i );
+			if( Util.stringContainsAll( s, match ) == shouldmatch ) {
+				if( length != -1 && s.length( ) != length ) continue;
+				
+				return s;
+			}
+		}
+		
+		throw new RuntimeException( "No segment matched: " + match );
+	}
+	
+	/**
+	 * Decodes a given array of segment encodings into an integer value
+	 * 
+	 * @param segments The array of encoded segments to decode
+	 * @return The decoded integer from the list of digit encodings
+	 */
+	public long decode( final String... segments ) {
+		long value = 0;
+		int d = 1;
+		
+		// do a backwards loop to reconstruct per decimal
+		for( int i = segments.length - 1; i >= 0; i-- ) {
+			value += decodeSegment( segments[i] ) * d;
+			d *= 10;
+		}
+		return value;
+	}
+	
+	/**
+	 * Decodes a single digit
+	 * 
+	 * @param in The segment encoding of the digit
+	 * @return The digit that it encodes
+	 */
+	protected long decodeSegment( final String in ) {		
+		for( int i : digits.keySet( ) ) {
+			final String dstr = digits.get( i );
+			if( in.length( ) == dstr.length( ) &&  Util.stringContainsAll( in, dstr ) ) return i;
+		}
+		
+		throw new RuntimeException( "Failed to decode digit from input " + in );
+	}
+	
+	
+	/**
+	 * @return The string that describes the current mapping 
+	 */
+	@Override
+	public String toString( ) {
+		return input.toString( );
+	}
+}
