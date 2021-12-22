bca564029a20732d4b787f1bf6db0e53f1123ad6
jscharpff <jcdscharpff@gmail.com>
2021-12-22 17:49:04 +0100

    Challenge day 22

---
+++ src/challenges/day22/Day22.java
@@ -0,0 +1,46 @@
+package challenges.day22;
+
+import java.util.List;
+
+import util.geometry.Cube3D;
+import util.io.FileReader;
+
+public class Day22 {
+
+	/**
+	 * Day 22 of the Advent of Code 2021
+	 * 
+	 * https://adventofcode.com/2021/day/22
+	 * 
+	 * @param args The command line arguments
+	 * @throws Exception
+	 */
+	public static void main( final String[] args ) throws Exception {
+		final List<String> ex_input = new FileReader( Day22.class.getResource( "day22_example.txt" ) ).readLines();
+		final List<String> ex2_input = new FileReader( Day22.class.getResource( "day22_example2.txt" ) ).readLines();
+		final List<String> input = new FileReader( Day22.class.getResource( "day22_input.txt" ) ).readLines( );
+		
+		System.out.println( "---[ Part 1 ]---" );
+		System.out.println( "Example: " + getActiveCubes( ex_input, new Cube3D( 50 ) ) );
+		System.out.println( "Answer : " + getActiveCubes( input, new Cube3D( 50 ) ) );
+
+		System.out.println( "\n---[ Part 2 ]---" );
+		System.out.println( "Example: " + getActiveCubes( ex2_input, null ) );
+		System.out.println( "Answer : " + getActiveCubes( input, null ) );
+	}
+	
+	/**
+	 * Initiates the boot sequence of the Submarine's reactor by feeding it the
+	 * list of boot instructions
+	 * 
+	 * @param input The set of booting sequence instructions to turn reactor
+	 *   cubes on or off
+	 * @param bootregion The region to initiate the boot sequence within
+	 * @return The number of cubes that are now active in the reactor as a
+	 *   result of completing the boot sequence
+	 */
+	public static long getActiveCubes( final List<String> input, final Cube3D bootregion ) {
+		final ReactorCore core = ReactorCore.fromBootingSequence( bootregion, input );
+		return core.getActivatedCubes( );
+	}
+}
---
+++ src/challenges/day22/ReactorCore.java
@@ -0,0 +1,99 @@
+package challenges.day22;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import util.geometry.Cube3D;
+
+/**
+ * Class that models the submarine reactor's Core, consisting of cubes in a
+ * three-dimensional integer space that may be activated or not. 
+ *  
+ * @author Joris
+ */
+public class ReactorCore {
+	/** The map that contains the regions of cubes that are on  */
+	private Set<Cube3D> regions;
+	
+	/**
+	 * Creates a new reactor core
+	 */
+	private ReactorCore( ) {
+		this.regions = new HashSet<>( );
+	}
+	
+	/**
+	 * Turns on the cubes in the specified region to on or off
+	 * 
+	 * @param region The region to set value of
+	 * @param value True to activate the cubes in the area, false to deactivate
+	 */
+	private void set( final Cube3D region, final boolean value ) {
+		// reconstruct set of active cubes
+		final Set<Cube3D> newset = new HashSet<>( );
+		
+		// check with all existing regions if they overlap and make sure to keep
+		// only non-overlapping areas
+		for( final Cube3D cube : regions ) {
+			if( !region.overlaps( cube ) )
+				// no overlap means no change 
+				newset.add( cube );
+			else
+				// if they do overlap, remove the new region from the cube and add
+				// the result of the subtraction to the set
+				newset.addAll( cube.subtract( region ) );
+		}		
+		
+		// if the region should active cubes add it and swap the sets
+		if( value ) newset.add( region );		
+		regions = newset;
+	}
+	
+	/**
+	 * Counts the number of set cubes int the specified region
+	 * 
+	 * @param region The region to consider 
+	 * @return The number of activated cubes in the given region 
+	 */
+	public long getActivatedCubes( ) {
+		long count = 0;
+		for( final Cube3D c : regions ) {
+			count += c.volume( );
+		}
+		return count;
+	}
+
+	/**
+	 * @return The regions of cubes that are currently active in the reactor core 
+	 */
+	@Override
+	public String toString( ) {
+		return regions.toString( );
+	}
+
+	/**
+	 * Creates a new reactor core from a booting sequence
+	 * 
+	 * @param bootregion The boot region, null for unbounded
+	 * @param sequence The list of boot instructions
+	 */
+	public static ReactorCore fromBootingSequence( final Cube3D bootregion, final List<String> sequence ) {
+		final ReactorCore r = new ReactorCore( );
+		
+		// parse sequence of instructions
+		for( final String seq : sequence ) {
+			final String[] s = seq.split( " " );
+			final boolean on = s[0].equals( "on" );
+			final Cube3D region = Cube3D.fromString( s[1] );
+			
+			// check if the region is within the boot region, otherwise discard it
+			if( bootregion != null && !region.overlaps( bootregion ) ) continue;
+						
+			// and apply the instruction
+			r.set( region, on );
+		}
+		
+		return r;
+	}
+}
